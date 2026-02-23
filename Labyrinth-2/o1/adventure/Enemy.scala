8f8e864d404eb2f3795b44a040c446c189c3ef99
Daud110 <120931211+Daud110@users.noreply.github.com>
2026-02-23 17:07:26 +0800

    Delete Labyrinth-2 directory

--- Labyrinth-2/o1/adventure/Enemy.scala
+++
@@ -1,6 +0,0 @@
-package o1.adventure
-
-class Enemy (val name: String, val description: String):
-
-  /** Returns a short textual representation of the item (its name, that is). */
-  override def toString = this.name
